8ce4b106a24748fdc55a671503c5ddae930024ff
Levan Roinishvili <levanroinishvili@gmail.com>
2025-02-06 15:46:02 +0000

    Simple Web Sockets

---
+++ README.md
@@ -0,0 +1,19 @@
+# Very Simple Tester for Web Sockets
+
+## Install
+
+* `git clone https://github.com/levanroinishvili/web-socket-tester.git`
+* `cd web-socket-tester`
+* `npm install`
+
+## Run
+
+* `node index.js`
+* Select the port and press enter
+* Connect from client (See [Connecting from Browser](#connecting-from-browser) for an example)
+* Connection information and messages will appear in the console
+* Type in console a message and press Enter to send to all connected WebSockets
+
+## Connecting from Browser
+
+To test sockets from the browser, open the included file [websocket.html](websocket.html) in the browser. Use buttons `Connect` and `Send to All`.
---
+++ index.js
@@ -0,0 +1,46 @@
+import { WebSocketServer } from 'ws';
+import chalk from 'chalk';
+import readline from 'node:readline';
+
+/* Ask user for the TCP port */
+const DEFAULT_PORT = 8080
+const port = await prompt(`Select port: ${chalk.grey(8080)} `) || DEFAULT_PORT
+
+const wss = new WebSocketServer({ port });
+console.log(chalk.green(`Listening on port`), port)
+
+let connectedSockets = []
+
+wss.on('connection', ws => {
+    connectedSockets = [...connectedSockets, ws]
+    console.log(chalk.green(`Connection received on port ${port}`))
+    ws.on('message', data => {
+        console.log()
+        console.log('Message:\n%s', chalk.yellow(data));
+    });
+    ws.onclose = () => {
+        console.log(chalk.red(`Closed connection on port`), port)
+        connectedSockets = connectedSockets.filter(w => w !== ws)
+    }
+});
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+reader.on('line', line => connectedSockets.forEach(socket => socket.send(line)))
+
+/** Will ask a question to user on console and await for a single line or reply */
+async function prompt(question) {
+    return new Promise((resolve, reject) => {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+        rl.question(question, response => {
+            rl.close()
+            resolve(response)
+        })
+    })
+}
